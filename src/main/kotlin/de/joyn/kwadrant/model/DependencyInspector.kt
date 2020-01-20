a12331a8304560b3257e317ba6efd7bc86a378a3
Michael Marth <michael.marth@joyn.de>
2020-01-20 12:18:42 +0100

    added expression function return types for readability

--- src/main/kotlin/de/joyn/kwadrant/model/DependencyInspector.kt
+++ src/main/kotlin/de/joyn/kwadrant/model/DependencyInspector.kt
@@ -3,11 +3,12 @@ package de.joyn.kwadrant.model
 import org.gradle.api.Project
 import org.gradle.api.artifacts.Dependency
 import org.gradle.api.artifacts.ExternalModuleDependency
+import org.gradle.api.internal.artifacts.dependencies.DefaultProjectDependency
 
 class DependencyInspector {
 
-    fun getDuplicatedDependencies(kwadrant: KwadrantInfo) =
-        kwadrant.local.libDependencies.fold(mutableSetOf<Pair<Dependency, Set<Project>>>()) { acc, dep ->
+    fun getDuplicatedDependencies(kwadrant: KwadrantInfo):  Set<Pair<Dependency, Set<Project>>> =
+        kwadrant.local.libDependencies.fold(mutableSetOf()) { acc, dep ->
             val parentDeps = kwadrant.parent.libDependencies.filter { (_, parentDeps) ->
                 parentDeps.contains(dep)
             }.map { (parent, _) ->
@@ -19,14 +20,14 @@ class DependencyInspector {
             acc
         }
 
-    fun getVersionMisalignedDependencies(rootProject: Project) = with(
+    fun getVersionMisalignedDependencies(rootProject: Project): Set<Pair<ExternalModuleDependency, Project>> = with(
         rootProject.allprojects.map {
             it to KwadrantInfo.create(it, rootProject)
         }.map {
             it.first to it.second.local.libDependencies
         }
     ) {
-        fold(mutableSetOf<Pair<ExternalModuleDependency, Project>>()) { acc, d ->
+        fold(mutableSetOf()) { acc, d ->
             val (project, localDeps) = d
             val misAlignedDuplicates = localDeps.filter { localD ->
                 this.map { it.second }.flatten().filter { allD ->
@@ -42,19 +43,21 @@ class DependencyInspector {
         }
     }
 
-    fun getDuplicatedParents(kwadrant: KwadrantInfo, rootProject: Project) = kwadrant.run {
+    fun getDuplicatedParents(
+        kwadrant: KwadrantInfo,
+        rootProject: Project
+    ): Set<Pair<Project, List<DefaultProjectDependency>>> = kwadrant.run {
         val directParents = kwadrant.local.projectDependencies
         val transitiveParents = kwadrant.parent.projects.map {
             it to KwadrantInfo.create(it, rootProject).local.projectDependencies
         }
-        val q = transitiveParents.filter { (_, parentParents) ->
+        transitiveParents.filter { (_, parentParents) ->
             directParents.any {
                 parentParents.contains(it)
             }
         }.map { (parent, parentParents) ->
             parent to parentParents.filter { directParents.contains(it) }
         }.toSet()
-        q
     }
 
 }
